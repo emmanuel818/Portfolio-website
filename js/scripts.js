0d3ff4b9473cf30efd496bc0a014c8b4e6ee8ab7
emmanuel818 <emmanuelg8182001@gmail.com>
2022-02-02 12:51:16 -0600

    Update

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+(function () {
+  let form = document.querySelector('#contact-form');
+  let emailInput = document.querySelector('#contact-email');
+  let telephoneInput = document.querySelector('#contact-telephone');
+
+  function showErrorMessage(input, message) {
+    let container = input.parentElement;
+
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    }
+
+
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, 'E-mail is a required field.');
+      return false;
+    }
+
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, 'You must enter a valid e-mail address.');
+      return false;
+    }
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+
+  function validateTelephone() {
+    let value = telephoneInput.value;
+
+    if(!value) {
+      showErrorMessage(telephoneInput, 'Telephone is a required field.');
+      return false;
+    }
+
+    if (value.length < 9) {
+      showErrorMessage(telephoneInput, 'The telephone needs to be at least 8 characters long.');
+      return false;
+    }
+
+    showErrorMessage(telephoneInput, null);
+    return true;
+  }
+
+  function validateForm() {
+    let isValidEmail = validateEmail();
+    let is validateTelephone = validateTelephone();
+    return isValidEmail && isValidTelephone;
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      alert('Sucess!');
+    }
+  });
+
+  emailInput.addEventListener('input', validateEmail);
+  telephoneInput.addEventListener('input',telephonePassword);
+})();
